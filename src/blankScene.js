30c17d1bfe2a3a3135c7bb0453073c832bf864b0
sawaura <sawaura@sw.it.aoyama.ac.jp>
2019-12-31 02:32:12 +0900

    feat : セーブデータIOをマージ

--- src/blankScene.js
+++ src/blankScene.js
@@ -1,3 +1,4 @@
+
 var blankScene = cc.Scene.extend({
     onEnter: function () {
         this._super();
@@ -8,9 +9,17 @@ var blankScene = cc.Scene.extend({
             cc.color(255, 0, 0, 20));
         this.addChild(background, 0);
 
+        // どうにかする
+        write_savedata(make_new_savedata_json());
+        var savedata = read_savedata();
+
         var label = cc.LabelTTF.create("Idle Game Center", "Arial", 120);
         label.setPosition(size.width / 2, size.height - 80);
         label.setFontFillColor(cc.color(0, 0, 0, 255));
         this.addChild(label, 1);
+        var coinInfo = cc.LabelTTF.create(`${savedata.coins} coins`, "Arial", 80);
+        coinInfo.setPosition(size.width / 2, size.height - 200);
+        coinInfo.setFontFillColor(cc.color(0, 0, 0, 255));
+        this.addChild(coinInfo, 1);
     }
 });
