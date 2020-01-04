c7a8ba6338a7754213fdbbd1ce139a9bfa01efc5
Kakeru Ito <konpeist@gmail.com>
2020-01-05 03:58:06 +0900

    fix:レイヤー

--- src/baseScene.js
+++ src/baseScene.js
@@ -2,7 +2,7 @@ var baseScene = cc.Scene.extend({
     onEnter: function () {
         this._super();
         this.addChild(new blankLayer(), 0);
-        this.addChild(new gameLayer(), 0);
-        this.addChild(new uiLayer(), 0);
+        this.addChild(new gameLayer(), 1);
+        this.addChild(new uiLayer(), 2);
     }
 });
