3dfdc6be91b20270532aa0b1a8030be8fea658db
pulcherriman <tsukasawa_agu@yahoo.co.jp>
2022-11-01 22:23:58 +0900

    re-initial commit.

--- src/baseScene.js
+++
@@ -1,8 +0,0 @@
-var baseScene = cc.Scene.extend({
-    onEnter: function () {
-        this._super();
-        this.addChild(new blankLayer(), 0);
-        this.addChild(new gameLayer(), 1);
-        this.addChild(new uiLayer(), 2);
-    }
-});
--- src/savedata.js
+++
@@ -1,4 +0,0 @@
-//もう使わないで
-function read_savedata(){
-    return JSON.parse(localStorage.getItem('dummy_save'));
-}
